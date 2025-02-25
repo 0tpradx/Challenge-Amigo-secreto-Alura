34edfcef2e85f612bf20debd363b2c3a87610011
Thamyres Valentim Prado <thamyres.vprado@gmail.com>
2025-02-25 15:12:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+  let amigo = document.querySelector('input').value;
+  if(!amigo){
+      Swal.fire({
+          title: "Erro ao inserir nome!",
+          text: "Por favor insira um nome válido.",
+          icon: "error",
+          confirmButtonText: "OK",
+          timer: 3000
+        });
+        return;
+
+  }
+  for(i=0;i<listaAmigos.length;i++){
+      if(amigo == listaAmigos [i]){
+          Swal.fire({
+              title: "Nome já inseerido!",
+              text: "Se for outra pessoa por favor colocar o sobrenome",
+              icon: "error",
+              confirmButtonText: "OK",
+              timer: 3200
+            });
+            return;
+      }
+  } 
+  
