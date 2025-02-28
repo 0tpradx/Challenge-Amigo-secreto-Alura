87f1303ac8095c563e9b82a27829ad8ebfc5b140
Thamyres Valentim Prado <thamyres.vprado@gmail.com>
2025-02-28 14:10:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,49 @@ function adicionarAmigo() {
 
 function exibirAmigos() {
     let listaHTML = document.querySelector('listaAmigos');
-    listaHTML
+    listaHTML.innerHTML = "";
+
+  for (let i=0; i < listaAmigos.length; i++) {
+      let item = document.createElement('li');  
+      item.textContent = listaAmigos[i];
+      listaHTML.appendChild(item);
+  }
+}
+function exibeSorteado(tag,texto){
+      let campo = document.querySelector(tag);
+      campo.innerHTML = texto;
+}
+
+
+function sortearAmigo(){
+      let amigoSorteado = listaAmigos[Math.floor(Math.random()* listaAmigos.length)];
+      //testes
+      //console.log(amigoSorteado);
+      //console.log(listaAmigos);
+      exibeSorteado('#resultado',`O amigo sorteado é: ${amigoSorteado} `);
+       document.getElementById('sortearAmigo').setAttribute('disabled', true);
+       document.getElementById('novoSorteio').disabled = false;
+       document.getElementById('button-add').setAttribute('disabled', true);
+       document.getElementById('amigo').setAttribute('disabled', true);
+}
+function limparCampo(){
+      amigo = document.querySelector('input');
+      amigo.value = '';
+}
+function novoSorteio(){
+      let listaHTML = document.querySelector('listaAmigos');
+      listaHTML.innerHTML = "";
+      exibeSorteado('#resultado','');
+      limparCampo();
+      document.getElementById('button-add').disabled = false;
+      document.getElementById('amigo').disabled = false;
+      document.getElementById('novoSorteio').setAttribute('disabled', true);
+      listaAmigos = [];
+
+}
+  
+          
+
     
   
   
